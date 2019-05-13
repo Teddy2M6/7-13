1e6ace0a0cf9012a0deadd1963eccd30836af477
Teddy2M6 <47573307+Teddy2M6@users.noreply.github.com>
2019-05-13 17:31:43 -0400

    Add files via upload

---
+++ main.lua
@@ -0,0 +1,12 @@
+-----------------------------------------------------------------------------------------
+--
+-- Created by: Teddy Sannan
+-- Created on: May 7, 2019
+--
+-- This file plays with scenes
+-----------------------------------------------------------------------------------------
+
+local composer = require( "composer" )
+ 
+
+composer.gotoScene( "scene1")
